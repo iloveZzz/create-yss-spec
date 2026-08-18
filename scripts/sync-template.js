5b59951435c9b8de209c30d8fb4173fa2425f09e
ming <daomingzhu@icloud.com>
2026-08-18 09:50:54 +0800

    chore: pin verified template release

--- scripts/sync-template.js
+++ scripts/sync-template.js
@@ -12,7 +12,7 @@ const targetSnapshotPath = path.join(packageRoot, "template.snapshot.json");
 const templateRepo =
   process.env.YSS_SPEC_TEMPLATE_REPO ||
   "https://github.com/iloveZzz/yss-spec-project-template.git";
-const DEFAULT_TEMPLATE_REF = "07a4eda35bd5cddfdf6511c79dce05fca6311d5d";
+const DEFAULT_TEMPLATE_REF = "772758e064889829576d55fd20b17421d98982a8";
 const templateRef = process.env.YSS_SPEC_TEMPLATE_REF || DEFAULT_TEMPLATE_REF;
 const NPM_IGNORED_BASENAMES = new Set([".gitignore", ".npmignore", ".npmrc"]);
 
