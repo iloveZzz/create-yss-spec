783b13f7b2f61f21edf6371006d7019a6043f0df
ming <daomingzhu@icloud.com>
2026-09-01 22:09:56 +0800

    chore(template): pin archify template

--- scripts/sync-template.js
+++ scripts/sync-template.js
@@ -12,7 +12,7 @@ const targetSnapshotPath = path.join(packageRoot, "template.snapshot.json");
 const templateRepo =
   process.env.YSS_SPEC_TEMPLATE_REPO ||
   "https://github.com/iloveZzz/yss-spec-project-template.git";
-const DEFAULT_TEMPLATE_REF = "76cd738a3bdd0b345d4fe1623bea360d30b9a71f";
+const DEFAULT_TEMPLATE_REF = "f4ee19614b6206e08d47d333b08e49d4f2af93c4";
 const templateRef = process.env.YSS_SPEC_TEMPLATE_REF || DEFAULT_TEMPLATE_REF;
 const NPM_IGNORED_BASENAMES = new Set([".gitignore", ".npmignore", ".npmrc"]);
 
