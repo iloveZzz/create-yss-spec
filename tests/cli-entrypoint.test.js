5ee12d15c6d431234f418374d9511805cf710bf8
ming <daomingzhu@icloud.com>
2026-09-05 22:26:27 +0800

    test(commands): cover command adapter seams

--- tests/cli-entrypoint.test.js
+++ tests/cli-entrypoint.test.js
@@ -4,7 +4,18 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 
 const modularCli = require("../src/cli/index.js");
+const { runAttach } = require("../src/commands/attach");
+const { runInit } = require("../src/commands/init");
+const { runSync } = require("../src/commands/sync");
+const { runUpdateCommand } = require("../src/commands/update");
 
 test("modular CLI entry exposes runCli", () => {
   assert.equal(typeof modularCli.runCli, "function");
+});
+
+test("command adapters expose stable execution seams", () => {
+  assert.equal(typeof runInit, "function");
+  assert.equal(typeof runAttach, "function");
+  assert.equal(typeof runSync, "function");
+  assert.equal(typeof runUpdateCommand, "function");
 });
