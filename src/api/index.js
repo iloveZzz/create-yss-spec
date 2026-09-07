5eaa0f9c4dd9c6c09fc7bf475956a42cf85bbd5a
ming <daomingzhu@icloud.com>
2026-09-07 09:15:09 +0800

    feat(api): expose stable error envelope helper

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { normalizeError } = require("../cli/error-output");
 const { projectDoctor } = require("./project-doctor");
 const { projectDiff } = require("./project-diff");
 const { templatePlan } = require("./template-plan");
@@ -7,10 +8,15 @@ const { templateApply } = require("./template-apply");
 
 const API_VERSION = 1;
 
+function toErrorEnvelope(error) {
+  return normalizeError(error);
+}
+
 module.exports = {
   API_VERSION,
   projectDoctor,
   projectDiff,
   templatePlan,
   templateApply,
+  toErrorEnvelope,
 };
