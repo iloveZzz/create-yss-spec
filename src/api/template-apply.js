0ccab53e7a9f4944ca59c43f0d4b6ea0c6414ac6
ming <daomingzhu@icloud.com>
2026-09-07 09:11:55 +0800

    feat(api): add stable templateApply API

---
+++ src/api/template-apply.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const { buildSyncContext, applySyncContext } = require("./_sync-service");
+
+function templateApply(options = {}) {
+  const context = buildSyncContext(options);
+  return applySyncContext(context, { force: Boolean(options.force) });
+}
+
+module.exports = { templateApply };
