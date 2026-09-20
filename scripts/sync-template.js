86adf8f04836e2d6f8c96890b2c4d73f26262b2a
ming <daomingzhu@icloud.com>
2026-09-20 23:39:57 +0800

    chore(cli): upgrade to 3.4.9 with committed skills template

--- scripts/sync-template.js
+++ scripts/sync-template.js
@@ -26,7 +26,7 @@ function isLocalRepo(value) {
 const templateRepo =
   process.env.YSS_SPEC_TEMPLATE_REPO ||
   (isLocalRepo(siblingHarness) ? siblingHarness : defaultRemote);
-const DEFAULT_TEMPLATE_REF = "60838f5213d420668c90cdfb2fe9ef19eee6f1a4";
+const DEFAULT_TEMPLATE_REF = "9126b41c5657b8c80ac3ff6aa7117f4b01e14e18";
 const templateRef =
   process.env.YSS_SPEC_TEMPLATE_REF ||
   (requireCommitted
